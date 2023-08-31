9f9c9be238e679b9b413744c640184ba18d967e8
jeunh <gkwldms1218@gmail.com>
2023-08-31 22:52:03 +0900

    TodoHeader 컴포넌트 구현

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
+</template>
+
+<!-- scoped: vue 싱글 파일 컴포넌트에서 지원하는 속성
+해당 컴포넌트 안에서만 유효한 스타일 속성 -->
+<style scoped>
+h1 {
+  color: #2F3B52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
