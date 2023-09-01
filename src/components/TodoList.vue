d8ec7367d8c44cf30bf0803431b45864288eeb33
jeunh <gkwldms1218@gmail.com>
2023-09-01 23:36:11 +0900

    TodoList 컴포넌트 할 일 완료 기능 구현

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
-        {{ todoItem }}
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+        <i class="checkBtn fa-solid fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" 
+          v-on:click="toggleComplete(todoItem, index)"></i>
+        <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
         <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
           <i class="fa-solid fa-trash-can"></i>
         </span>
@@ -22,15 +24,21 @@ export default {
     if(localStorage.length > 0) {
       for(var i=0 ; i<localStorage.length ; i++) {
         if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          this.todoItems.push(localStorage.key(i));
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
         }
       }
     }
   },
   methods: {
     removeTodo: function(todoItem, index) {
-      localStorage.removeItem(todoItem);
+      localStorage.removeItem(todoItem.item);
       this.todoItems.splice(index, 1);
+    },
+    toggleComplete: function(todoItem, index) {
+      todoItem.completed = !todoItem.completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+      console.log(index);
     }
   }
 }
@@ -58,9 +66,9 @@ export default {
     color: #de4343;
   }
   .checkBtn {
-    line-height: 45px;
+    line-height: 50px;
     color: #62acde;
-    margin-right: 5px;
+    margin-right: 8px;
   }
   .checkBtnCompleted {
     color: #b3adad;
